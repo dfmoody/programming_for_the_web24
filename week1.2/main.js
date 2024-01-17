1208d08d2a6bc78c53455607a4b0d790e3b457c2
dfmoody <150189905+dfmoody@users.noreply.github.com>
2024-01-17 14:00:05 -0700

    new functions

--- week1.2/main.js
+++ week1.2/main.js
@@ -1,3 +1,16 @@
-const name = window.prompt('What is your name?');
-console.log('Hello World!');
-console.log('Hello, ' + name + '!');
+function setup() {
+    createCanvas(500, 500)
+}
+function draw() {
+    if (mouseIsPressed) {
+        fill("maroon");
+        stroke("red")
+        rect(mouseX, mouseY, 40, 80)
+    }   
+    else {
+        fill("crimson");
+        stroke("pink");
+        rect(mouseX, mouseY, 50, 60)
+    }
+    
+}
