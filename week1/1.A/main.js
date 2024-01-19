b0b3b30df8e605acc430daf43b5ed6bf14096cd7
dfmoody <150189905+dfmoody@users.noreply.github.com>
2024-01-19 12:11:10 -0700

    added prompts

--- week1/1.A/main.js
+++ week1/1.A/main.js
@@ -1,15 +1,19 @@
 let grid = undefined
 
-let stroke1 = prompt("enter a basic color name in lower case", "gray");
-let stroke2 = prompt("enter another basic color name in lower case", "purple");
+let stroke1 = prompt("enter a basic color name in lower case (scarf color)", "red");
+let stroke2 = prompt("enter another basic color name in lower case (hat ribbon)", "yellow");
 
 function setup () {
     createCanvas(1000, 800);
-    // background("#ccc");
+    background("lavender");
     // grid = loadImage("img/100px_grid.png");
 }
 function draw() {
-    // background(grid);
+    // background
+    fill("#f1f1f1");
+    strokeWeight(20);
+    stroke("black");
+    rect(-10,650,1020,160)
     // snowman legs
     fill("#f1f1f1");
     strokeWeight(20);
@@ -20,11 +24,11 @@ function draw() {
     ellipse(600, 650, 200);
     // body
     ellipse(500, 450, 350, 400);
-    // hat brim highlight
-    stroke("red");
+    // scarf
+    stroke(stroke1);
     strokeWeight(50);
-    line(400, 310, 600, 310);
-    line(580, 310, 590, 360);
+    line(410, 300, 600, 300);
+    line(580, 310, 610, 360);
     // head
     stroke("lightblue");
     strokeWeight(20);
@@ -40,8 +44,14 @@ function draw() {
     strokeWeight(50);
     point(465, 180);
     point(535, 180);
+    // eyes pupils
+    stroke("white");
+    strokeWeight(15);
+    point(455, 180);
+    point(525, 180);
     // mouth
     noFill();
+    stroke(0);
     strokeWeight(10);
     arc(500, 230, 70, 50, 0, 2.9)
     // buttons
@@ -51,7 +61,7 @@ function draw() {
     point(500, 440);
     point(500, 520);
     // hat brim highlight
-    stroke("yellow");
+    stroke(stroke2);
     strokeWeight(20);
     line(430, 90, 570, 90);
     // arms
@@ -67,4 +77,41 @@ function draw() {
     stroke("orange");
     strokeWeight(20);
     line(495, 220, 560, 225);
+
+     // small body
+     fill("#f1f1f1");
+     strokeWeight(20);
+     stroke("lightblue");
+     ellipse(150, 600, 200, 300);
+     // head
+     stroke("lightblue");
+     strokeWeight(20);
+     ellipse(150, 400, 150);
+    // hat brim
+    stroke("black");
+    strokeWeight(40);
+    line(90, 330, 210, 330);
+    // hat body
+    quad(110, 330, 110, 280, 190, 280, 190, 330);
+     // eyes
+     stroke(0);
+     strokeWeight(30);
+     point(115, 380);
+     point(180, 380);
+     // eyes pupils
+     stroke("white");
+     strokeWeight(10);
+     point(119, 380);
+     point(184, 380);
+    // mouth
+    noFill();
+    stroke(0);
+    strokeWeight(10);
+    arc(150, 410, 70, 30, 0.5, 3)
+    // buttons
+    stroke(0);
+    strokeWeight(30);
+    point(150, 520);
+    point(150, 570);
+    point(150, 620);
 }
