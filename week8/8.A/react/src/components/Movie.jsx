4831fca34f444d4338d54f7765482f8939d04126
dfmoody <150189905+dfmoody@users.noreply.github.com>
2024-03-06 14:00:06 -0700

    added Assignment 8.A

---
+++ week8/8.A/react/src/components/Movie.jsx
@@ -0,0 +1,23 @@
+const MovieCollection = (props) => {
+  // Ternary condition ? valueIfTrue : valueIfFalse
+  // If index is divisible by 2 that make it "even-row", if not make it "odd-row"
+  const rowClass = props.index % 2 === 0 ? 'even-row' : 'odd-row';
+
+  // return Table Row (with variable className)
+  // include table data for properties: name, year, genre and description
+  return (
+    <tr className={rowClass}>
+      <td className="movieName">{props.name}</td>
+      <td>{props.year}</td>
+      <td>{props.genre}</td>
+      <td><img className='movie-image' src={props.image}></img></td>
+      <td>{props.description}</td>
+      <td>
+        <button onClick={props.onDelete}>Delete</button> {/* Delete button */}
+        <button onClick={props.onDuplicate}>Duplicate</button> {/* Duplicate button */}
+      </td>
+    </tr>
+  );
+};
+  
+export default MovieCollection;
