88543a85bfd2f17f899ae12ecfbfb396a49ee8d2
dfmoody <150189905+dfmoody@users.noreply.github.com>
2024-03-26 20:32:14 -0600

    fixed song.jsx and adding css styles

--- week10/ex2/src/views/Home.jsx
+++ week10/ex2/src/views/Home.jsx
@@ -1,5 +1,5 @@
-import musicData from "../assets/music-list.json";
 import {Link} from "react-router-dom";
+import musicData from "../assets/music-list.json";
 
 export function Home() {
     return (
@@ -9,7 +9,7 @@ export function Home() {
             return (
                 <div key={song.slug}>
                     <img src={song.cover} alt={song.title} />
-                    <p><Link to={`${song.slug}`}>{song.title} by {song.artist}</Link></p>
+                    <p><Link to={`${song.slug}`}><strong>{song.title}</strong> by {song.artist}</Link></p>
                 </div>
             )
         })}
--- week10/ex2/src/views/Song.jsx
+++ week10/ex2/src/views/Song.jsx
@@ -1,19 +1,21 @@
 import {Link} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export function Song({data}) {
     const {slug} = useParams();
     console.log("slug", slug);
-    const selectedSong = data.find((song) => song.slug === slug)
+    const selectedSong = data.find((song) => song.slug === slug);
     console.log(selectedSong)
+
     return(
         <>
         <nav>
-          <Link to="/">Go back Home</Link>
+            <Link to="/">Go back Home</Link>
         </nav>
         <h1>{selectedSong.title}</h1>
         <img src={selectedSong.cover} alt={selectedSong.title} />
         <p>Artist: {selectedSong.artist}</p>
-        <p>Artist: {selectedSong.rank}</p>
+        <p>Rank: {selectedSong.rank}</p>
         </>
     )
 }
